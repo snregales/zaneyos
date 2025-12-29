1d4f8672e805fd068c65365a165950c604b52543
Sharlon N. Regales <sharlonregales@gmail.com>
2025-12-29 22:13:41 +0100

    Move ternminal sub module integration to the terminal module
    Home module should not care/know about individual sub modules
    
    Signed-off-by: Sharlon N. Regales <sharlonregales@gmail.com>

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -2,12 +2,8 @@
   vars = import ../../hosts/${host}/variables.nix;
   inherit
     (vars)
-    alacrittyEnable
     barChoice
-    ghosttyEnable
-    tmuxEnable
     waybarChoice
-    weztermEnable
     ;
   # Select bar module based on barChoice
   barModule =
@@ -15,56 +11,35 @@
     then ./noctalia.nix
     else waybarChoice;
 in {
-  imports =
-    [
-      ./amfora.nix
-      ./cli
-      ./overview.nix
-      ./python.nix
-      ./emoji.nix
-      ./eza.nix
-      ./fastfetch
-      ./gtk.nix
-      ./hyprland
-      ./terminals/kitty.nix
-      ./obs-studio.nix
-      ./editors
-      ./rofi
-      ./qt.nix
-      ./scripts
-      ./scripts/gemini-cli.nix
-      ./starship-ddubs-1.nix
-      ./stylix.nix
-      ./swappy.nix
-      ./swaync.nix
-      ./tealdeer.nix
-      ./virtmanager.nix
-      barModule
-      ./wlogout
-      ./xdg.nix
-      ./yazi
-      ./zen-browser.nix
-      ./zoxide.nix
-      ./zsh.nix
-    ]
-    ++ (
-      if weztermEnable
-      then [./terminals/wezterm.nix]
-      else []
-    )
-    ++ (
-      if ghosttyEnable
-      then [./terminals/ghostty.nix]
-      else []
-    )
-    ++ (
-      if tmuxEnable
-      then [./terminals/tmux.nix]
-      else []
-    )
-    ++ (
-      if alacrittyEnable
-      then [./terminals/alacritty.nix]
-      else []
-    );
+  imports = [
+    ./amfora.nix
+    ./cli
+    ./overview.nix
+    ./python.nix
+    ./emoji.nix
+    ./eza.nix
+    ./fastfetch
+    ./gtk.nix
+    ./hyprland
+    ./terminals
+    ./obs-studio.nix
+    ./editors
+    ./rofi
+    ./qt.nix
+    ./scripts
+    ./scripts/gemini-cli.nix
+    ./starship-ddubs-1.nix
+    ./stylix.nix
+    ./swappy.nix
+    ./swaync.nix
+    ./tealdeer.nix
+    ./virtmanager.nix
+    barModule
+    ./wlogout
+    ./xdg.nix
+    ./yazi
+    ./zen-browser.nix
+    ./zoxide.nix
+    ./zsh.nix
+  ];
 }
---
+++ modules/home/terminals/default.nix
@@ -0,0 +1,35 @@
+{host, ...}: let
+  vars = import ../../../hosts/${host}/variables.nix;
+  inherit
+    (vars)
+    alacrittyEnable
+    ghosttyEnable
+    tmuxEnable
+    weztermEnable
+    ;
+in {
+  imports =
+    [
+      ./kitty.nix
+    ]
+    ++ (
+      if weztermEnable
+      then [./wezterm.nix]
+      else []
+    )
+    ++ (
+      if ghosttyEnable
+      then [./ghostty.nix]
+      else []
+    )
+    ++ (
+      if tmuxEnable
+      then [./tmux.nix]
+      else []
+    )
+    ++ (
+      if alacrittyEnable
+      then [./alacritty.nix]
+      else []
+    );
+}
